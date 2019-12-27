43b2487573e3f265665d9019acd909d87c908b50
yuzujoe <yuzun225@gmail.com>
2019-12-27 17:43:14 +0900

    unit test

---
+++ api/services/sort_service.go
@@ -0,0 +1,7 @@
+package services
+
+import "testing-go/api/utils/sort"
+
+func Sort(elements []int) {
+	sort.BubbleSort(elements)
+}
---
+++ api/services/sort_service_test.go
@@ -0,0 +1,17 @@
+package services
+
+import "testing"
+
+func TestSort(t *testing.T) {
+	elements := []int{7, 9, 5, 3, 1, 0, 4, 6, 8, 2}
+
+	Sort(elements)
+
+	if elements[0] != 9 {
+		t.Error("first elements should be 9")
+	}
+
+	if elements[len(elements)-1] != 0 {
+		t.Error("last element shoul be 0")
+	}
+}
